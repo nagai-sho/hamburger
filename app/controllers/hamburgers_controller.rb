230ede0f00aad336b5810bfacf4d1c765b5c471e
Nagai_sho <as.uland.dr@gmail.com>
2024-10-28 21:51:59 +0900

    first_commit

---
+++ app/controllers/hamburgers_controller.rb
@@ -0,0 +1,5 @@
+class HamburgersController < ApplicationController
+  def index
+    
+  end
+end
---
+++ test/controllers/hamburgers_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class HamburgersControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
